a21b59aaecbacc22df2e5eaa8ef3e80f16a65e77
tamaskis <tamas.a.kis@outlook.com>
2021-05-31 20:52:49 -0700

    New header format + example as live script.

--- ground_track.m
+++ ground_track.m
@@ -1,51 +1,38 @@
-% ground_track  Plots the ground track of an orbit given its planetodetic
-% coordinates (i.e. planetodetic latitude and longitude).
+%==========================================================================
 %
-%   ground_track(lat,lon) plots the ground track of an orbit around the
-%   Earth, superimposed on a map of the Earth, where the map just shows the
-%   continental outlines. "lat" and "lon" are vectors of geodetic latitude
-%   and longitude values. By default, the ground track is drawn in blue
-%   with a line width of 1.5.
+% ground_track  Plots the ground track of an orbit.
 %
-%   ground_track(lat,lon,color) does the same as the syntax above, but
-%   draws the ground track with the color specified by "color". By default,
-%   a line width of 1.5 is used.
-%
-%   ground_track(lat,lon,color,line_width) does the same as the previous
-%   syntaxes, but draws the ground track with the line width specified by
-%   "line_width". If you would only like to specify the line width, but not
-%   the color, use the syntax "ground_track(lat,lon,[],line_width)".
-%
-%   ground_track(__,planet) draws the ground track of an orbit around the
-%   celestial body specified by "planet". "planet' can be specified as 
-%   'Sun', 'Moon', 'Mercury', 'Venus', 'Earth', 'Earth Cloudy', 
-%   'Earth Night', 'Earth Night Cloudy', 'Earth Continents Outline', 
-%   'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', or 'Pluto'. Note that
-%   if you are specifying "planet", but choosing not to specify some other
-%   parameters, you have to replace the variables you skipped over with 
-%   "[]". For example, if you wanted to specify the color but not the line 
-%   width, you would use "ground_track(lat,lon,color,[],planet)".
+%   ground_track(lat,lon)
+%   ground_track(lat,lon,color)
+%   ground_track(lat,lon,[],line_width)
+%   ground_track(lat,lon,color,line_width)
+%   ground_track(__,planet)
 %
 % MATLAB Central File Exchange: https://www.mathworks.com/matlabcentral/fileexchange/89822-ground-track-ground_track
 % GitHub: https://github.com/tamaskis/ground_track-MATLAB
 %
 % See "DOCUMENTATION.pdf" for additional documentation and examples. 
-% Examples can also be found in EXAMPLES.m. Both of these files are 
+% Examples can also be found in EXAMPLES.mlx. Both of these files are 
 % included with the download.
 %
 % Copyright (c) 2021 Tamas Kis
-% Last Update: 2021-04-05
-
-
-
-%% FUNCTION
-
-% INPUT: lat - vector of geodetic latitudes
-%        lon - vector of geodetic longitudes
-%        color - (OPTIONAL) line color
-%        line width - (OPTIONAL) line width
-%        planet - (OPTIONAL) name of celestial body
-% OUTPUT: plot of the ground track
+% Last Update: 2021-05-31
+%
+%--------------------------------------------------------------------------
+%
+% INPUTS:
+%   lat         (nx1 or 1xn) [deg] planetodetic latitude
+%   lon         (nx1 or 1xn) [deg] planetodetic longitude
+%   color       (OPTIONAL) (3x1 or 1x3) [rgb] line color (defaults to
+%                                             [0,0.4470,0.7410])
+%   line_width  (OPTIONAL) (1x1) line width (defaults to 1.5)
+%   planet      (OPTIONAL) 'Sun', 'Moon', 'Mercury', 'Venus', 'Earth', 
+%                          'Earth Cloudy', 'Earth Night',
+%                          'Earth Night Cloudy', 
+%                          'Earth Continents Outline', 'Mars', 'Jupiter',
+%                          'Saturn', 'Uranus', 'Neptune', or 'Pluto'
+%
+%==========================================================================
 function ground_track(lat,lon,color,line_width,planet)
     
     % sets default color to [0,0.4470,0.7410]
