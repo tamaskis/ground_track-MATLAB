a98e87659776a7e88575e1441aec047770ac92ba
tamaskis <tamas.a.kis@outlook.com>
2021-07-09 18:13:03 -0400

    Added option to plot ground track over blank background, made function case-insensitive.

--- ground_track.m
+++ ground_track.m
@@ -8,8 +8,11 @@
 %   ground_track(lat,lon,color,line_width)
 %   ground_track(__,planet)
 %
+% See also planet3D.
+%
 % Copyright © 2021 Tamas Kis
-% Last Update: 2021-06-09
+% Contact: tamas.a.kis@outlook.com
+% Last Update: 2021-07-09
 %
 %--------------------------------------------------------------------------
 %
@@ -21,15 +24,15 @@
 %
 %--------------------------------------------------------------------------
 %
-% -------
-% INPUTS:
-% -------
+% ------
+% INPUT:
+% ------
 %   lat         - (n×1 or 1×n) [deg] planetodetic latitude
 %   lon         - (n×1 or 1×n) [deg] planetodetic longitude
 %   color       - (OPTIONAL) (3×1 or 1×3) [rgb] line color
 %   line_width  - (OPTIONAL) (1×1) line width
-%   planet      - (OPTIONAL) (char) 'Sun', 'Moon', 'Mercury', 'Venus', 
-%                 'Earth', 'Earth Cloudy', 'Earth Night', 
+%   planet      - (OPTIONAL) (char) 'Blank','Sun', 'Moon', 'Mercury', 
+%                 'Venus', 'Earth', 'Earth Cloudy', 'Earth Night', 
 %                 'Earth Night Cloudy', 'Earth Continents Outline', 'Mars',
 %                 'Jupiter', 'Saturn', 'Uranus', 'Neptune', or 'Pluto'
 %
@@ -46,13 +49,13 @@ function ground_track(lat,lon,color,line_width,planet)
         line_width = 1.5;
     end
     
-    % sets default planet to Earth (using only outlines of continents)
+    % sets default background to outlines of Earth continents
     if (nargin < 5) || isempty(planet)
         planet = 'Earth Continents Outline';
     end
-
+    
     % sets background of ground track plot
-    if strcmp(planet,'Earth Continents Outline')
+    if strcmpi(planet,'Earth Continents Outline')
         
         % loads Earth topographic data
         load('topo.mat','topo');
@@ -65,62 +68,64 @@ function ground_track(lat,lon,color,line_width,planet)
         % elevation of 0
         contour(-180:179,-90:89,topoplot,[0,0],'black');
         
-    else
+    elseif ~strcmpi(planet,'Blank')
         
         % reads in image
-        if strcmp(planet,'Earth Cloudy')
-            cdata = imread('Images/Earth.png')+imread('Images/Clouds.png');
-        elseif strcmp(planet,'Earth Night Cloudy')
-            cdata = imread('Images/Earth Night.png')+...
-                0.1*imread('Images/Clouds.png');
+        if strcmpi(planet,'Earth Cloudy')
+            cdata = imread('images/earth.png')+imread('images/clouds.png');
+        elseif strcmpi(planet,'Earth Night')
+            cdata = imread('images/earthnight.png');
+        elseif strcmpi(planet,'Earth Night Cloudy')
+            cdata = imread('images/earthnight.png')+0.1*...
+                imread('images/clouds.png');
         else
-            cdata = imread(strcat('Images/',planet,'.png'));
+            cdata = imread(strcat('images/',lower(planet),'.png'));
         end
         
         % plots background
         image('CData',flipud(cdata),'XData',[-180,180],'YData',[-90,90]);
         
         % determines grid style based on background
-        if strcmp(planet,'Sun')
+        if strcmpi(planet,'Sun')
             grid_line_width = 1;
             grid_color = 'k';
-        elseif strcmp(planet,'Moon')
+        elseif strcmpi(planet,'Moon')
             grid_line_width = 0.75;
             grid_color = 'y';
-        elseif strcmp(planet,'Mercury')
+        elseif strcmpi(planet,'Mercury')
             grid_line_width = 0.75;
             grid_color = 'y';
-        elseif strcmp(planet,'Venus')
+        elseif strcmpi(planet,'Venus')
             grid_line_width = 0.5;
             grid_color = 'k';
-        elseif strcmp(planet,'Earth')
+        elseif strcmpi(planet,'Earth')
             grid_line_width = 0.5;
             grid_color = 'y';
-        elseif strcmp(planet,'Earth Cloudy')
+        elseif strcmpi(planet,'Earth Cloudy')
             grid_line_width = 1.5;
             grid_color = 'y';
-        elseif strcmp(planet,'Earth Night')
+        elseif strcmpi(planet,'Earth Night')
             grid_line_width = 0.5;
             grid_color = [0.5,0.5,0.5];
-        elseif strcmp(planet,'Earth Night Cloudy')
+        elseif strcmpi(planet,'Earth Night Cloudy')
             grid_line_width = 0.5;
             grid_color = [0.5,0.5,0.5];
-        elseif strcmp(planet,'Mars')
+        elseif strcmpi(planet,'Mars')
             grid_line_width = 0.75;
             grid_color = 'y';
-        elseif strcmp(planet,'Jupiter')
+        elseif strcmpi(planet,'Jupiter')
             grid_line_width = 1;
             grid_color = 'k';
-        elseif strcmp(planet,'Saturn')
+        elseif strcmpi(planet,'Saturn')
             grid_line_width = 0.65;
             grid_color = 'k';
-        elseif strcmp(planet,'Uranus')
+        elseif strcmpi(planet,'Uranus')
             grid_line_width = 0.5;
             grid_color = 'k';
-        elseif strcmp(planet,'Neptune')
+        elseif strcmpi(planet,'Neptune')
             grid_line_width = 0.5;
             grid_color = 'w';
-        elseif strcmp(planet,'Pluto')
+        elseif strcmpi(planet,'Pluto')
             grid_line_width = 0.65;
             grid_color = 'g';
         end
@@ -159,16 +164,19 @@ function ground_track(lat,lon,color,line_width,planet)
         line_width);
     for i = 1:(length(j)-1)
         plot(lon(j(i):(j(i+1)-1)),lat(j(i):(j(i+1)-1)),'color',color,...
-           'linewidth',line_width,'HandleVisibility','off');
+           'linewidth',line_width,'handlevisibility','off');
     end
 
     % axis formatting
     axis equal
     grid on
-    ax = gca;
-    ax.GridColor = [0.35,0.35,0.35];
-    ax.GridAlpha = 1;
-    ax.GridLineStyle = ':';
+    if strcmpi(planet,'Earth Continents Outline') || strcmpi(planet,...
+            'Blank')
+        ax = gca;
+        ax.GridColor = [0.35,0.35,0.35];
+        ax.GridAlpha = 1;
+        ax.GridLineStyle = ':';
+    end
     xlim([-180,180]);
     xticks(-180:30:180);
     ylim([-90,90]);
