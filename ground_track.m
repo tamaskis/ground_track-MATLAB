d7e67444adee8a3fb1d36703d9193c20a19ac5b3
tamaskis <tamas.a.kis@outlook.com>
2021-12-12 16:06:07 -0800

    reorganizing directory sturcture

--- ground_track.m
+++ ground_track.m
@@ -9,7 +9,7 @@
 % See also planet3D.
 %
 % Copyright © 2021 Tamas Kis
-% Last Update: 2021-08-28
+% Last Update: 2021-12-12
 % Website: https://tamaskis.github.io
 % Contact: tamas.a.kis@outlook.com
 %
@@ -26,14 +26,14 @@
 % ------
 % INPUT:
 % ------
-%   lat     - (n×1 or 1×n double) planetodetic latitude [deg]
-%   lon     - (n×1 or 1×n double) planetodetic longitude [deg]
-%   opts    - (OPTIONAL) (struct) plot options structure
-%       • color         - (char or 1×3 double) line color
-%                           --> can be specified as a name, short name, or
-%                               RGB triplet [rgb]
-%       • line_width    - (1×1 double) line width
-%       • line_style	- (char) line style
+%   lat     - (1D double array) planetodetic latitude [deg]
+%   lon     - (1D double array) planetodetic longitude [deg]
+%   opts    - (OPTIONAL) (1×1 struct) plot options
+%       • Color     - (char or 1×3 double) line color
+%                       --> can be specified as a name, short name, or RGB 
+%                           triplet [rgb]
+%       • LineWidth - (1×1 double) line width
+%       • LineStyle	- (char) line style
 %   planet  - (OPTIONAL) (char) 'Blank', 'Sun', 'Moon', 'Mercury', 'Venus',
 %             'Earth', 'Earth Cloudy', 'Earth Coastlines', 'Earth Night', 
 %             'Earth Night Cloudy', 'Mars', 'Jupiter', 'Saturn', 'Uranus',
@@ -47,24 +47,24 @@ function ground_track(lat,lon,opts,planet)
     % ------------------------------------
     
     % sets line color (defaults to default MATLAB color)
-    if (nargin < 3) || isempty(opts) || ~isfield(opts,'color')
-        color = [0,0.4470,0.7410];
+    if (nargin < 3) || isempty(opts) || ~isfield(opts,'Color')
+        Color = [0,0.4470,0.7410];
     else
-        color = opts.color;
+        Color = opts.Color;
     end
     
     % sets line style (defaults to solid line)
-    if (nargin < 3) || isempty(opts) || ~isfield(opts,'line_style')
-        line_style = '-';
+    if (nargin < 3) || isempty(opts) || ~isfield(opts,'LineStyle')
+        LineStyle = '-';
     else
-        line_style = opts.line_style;
+        LineStyle = opts.LineStyle;
     end
     
     % sets line width (defaults to 1.5)
-    if (nargin < 3) || isempty(opts) || ~isfield(opts,'line_width')
-        line_width = 1.5;
+    if (nargin < 3) || isempty(opts) || ~isfield(opts,'LineWidth')
+        LineWidth = 1.5;
     else
-        line_width = opts.line_width;
+        LineWidth = opts.LineWidth;
     end
     
     % sets background (defaults to Earth coastlines)
@@ -109,58 +109,58 @@ function ground_track(lat,lon,opts,planet)
         
         % determines grid style based on background
         if strcmpi(planet,'Sun')
-            grid_line_width = 1;
-            grid_color = 'k';
+            GridLineWidth = 1;
+            GridColor = 'k';
         elseif strcmpi(planet,'Moon')
-            grid_line_width = 0.75;
-            grid_color = 'y';
+            GridLineWidth = 0.75;
+            GridColor = 'y';
         elseif strcmpi(planet,'Mercury')
-            grid_line_width = 0.75;
-            grid_color = 'y';
+            GridLineWidth = 0.75;
+            GridColor = 'y';
         elseif strcmpi(planet,'Venus')
-            grid_line_width = 0.5;
-            grid_color = 'k';
+            GridLineWidth = 0.5;
+            GridColor = 'k';
         elseif strcmpi(planet,'Earth')
-            grid_line_width = 0.5;
-            grid_color = 'y';
+            GridLineWidth = 0.5;
+            GridColor = 'y';
         elseif strcmpi(planet,'Earth Cloudy')
-            grid_line_width = 1.5;
-            grid_color = 'y';
+            GridLineWidth = 1.5;
+            GridColor = 'y';
         elseif strcmpi(planet,'Earth Night')
-            grid_line_width = 0.5;
-            grid_color = [0.5,0.5,0.5];
+            GridLineWidth = 0.5;
+            GridColor = [0.5,0.5,0.5];
         elseif strcmpi(planet,'Earth Night Cloudy')
-            grid_line_width = 0.5;
-            grid_color = [0.5,0.5,0.5];
+            GridLineWidth = 0.5;
+            GridColor = [0.5,0.5,0.5];
         elseif strcmpi(planet,'Mars')
-            grid_line_width = 0.75;
-            grid_color = 'y';
+            GridLineWidth = 0.75;
+            GridColor = 'y';
         elseif strcmpi(planet,'Jupiter')
-            grid_line_width = 1;
-            grid_color = 'k';
+            GridLineWidth = 1;
+            GridColor = 'k';
         elseif strcmpi(planet,'Saturn')
-            grid_line_width = 0.65;
-            grid_color = 'k';
+            GridLineWidth = 0.65;
+            GridColor = 'k';
         elseif strcmpi(planet,'Uranus')
-            grid_line_width = 0.5;
-            grid_color = 'k';
+            GridLineWidth = 0.5;
+            GridColor = 'k';
         elseif strcmpi(planet,'Neptune')
-            grid_line_width = 0.5;
-            grid_color = 'w';
+            GridLineWidth = 0.5;
+            GridColor = 'w';
         elseif strcmpi(planet,'Pluto')
-            grid_line_width = 0.65;
-            grid_color = 'g';
+            GridLineWidth = 0.65;
+            GridColor = 'g';
         end
         
         % manually adds grid
         hold on;
         for i = 1:11
-            plot([-180+i*30,-180+i*30],[-90,90],'color',grid_color,...
-                'linewidth',grid_line_width,'linestyle',':');
+            plot([-180+i*30,-180+i*30],[-90,90],'Color',GridColor,...
+                'LineWidth',GridLineWidth,'LineStyle',':');
         end
         for i = 1:5
-            plot([-180,180],[-90+i*30,-90+i*30],'color',grid_color,...
-                'linewidth',grid_line_width,'linestyle',':');
+            plot([-180,180],[-90+i*30,-90+i*30],'color',GridColor,...
+                'LineWidth',GridLineWidth,'LineStyle',':');
         end
     
     end
@@ -186,12 +186,12 @@ function ground_track(lat,lon,opts,planet)
     % plots groundtrack (starts new plot every time ground track crosses
     % left border or right border)
     hold on;
-    plot(lon(1:(j(1)-1)),lat(1:(j(1)-1)),'color',color,'linestyle',...
-        line_style,'linewidth',line_width);
+    plot(lon(1:(j(1)-1)),lat(1:(j(1)-1)),'Color',Color,'LineStyle',...
+        LineStyle,'LineWidth',LineWidth);
     for i = 1:(length(j)-1)
-        plot(lon(j(i):(j(i+1)-1)),lat(j(i):(j(i+1)-1)),'color',color,...
-           'linestyle',line_style,'linewidth',line_width,...
-           'handlevisibility','off');
+        plot(lon(j(i):(j(i+1)-1)),lat(j(i):(j(i+1)-1)),'Color',Color,...
+           'LineStyle',LineStyle,'LineWidth',LineWidth,...
+           'HandleVisibility','off');
     end
 
     % axis formatting
@@ -208,9 +208,9 @@ function ground_track(lat,lon,opts,planet)
     xticks(-180:30:180);
     ylim([-90,90]);
     yticks(-90:30:90);
-    xlabel('Longitude, $\lambda\;[^{\circ}]$','interpreter','latex',...
-        'fontsize',18);
-    ylabel('Latitude, $\phi\;[^{\circ}]$','interpreter','latex',...
-        'fontsize',18);
+    xlabel('Longitude, $\lambda\;[^{\circ}]$','Interpreter','latex',...
+        'FontSize',18);
+    ylabel('Latitude, $\phi\;[^{\circ}]$','Interpreter','latex',...
+        'FontSize',18);
     
 end
